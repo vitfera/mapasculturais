380eaccd69c7a948fc05b81c40e4adbf03d8b861
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-09-20 10:23:48 -0300

    feat: remove opção de torna-lo modelo público

--- src/modules/Panel/components/panel--entity-models-card/template.php
+++ src/modules/Panel/components/panel--entity-models-card/template.php
@@ -69,16 +69,7 @@ $this->import('
                     <mc-icon name="agent" class="icon-model"></mc-icon>
                     <strong><?=i::__('Tipo de agente: ')?></strong> {{ model.tipoAgente }}
                     <br><br>
-                    <?php if($app->user->is('admin')): ?>
-                        <div v-if="entity.currentUserPermissions?.modify">
-                            <label class="switch" >
-                                <input type="checkbox" v-model="isModelPublic" />
-                                <span class="slider round"></span>
-                            </label>
-                            <span class="switch-text"><?= i::__("Modelo público") ?></span>
-                        </div>
-                        <br><br>
-                    <?php endif; ?>
+                    
                 </span>
             </div>
         </div>
