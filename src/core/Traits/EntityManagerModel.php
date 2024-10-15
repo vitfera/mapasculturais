df6b9dd4b38e72625096ab99cbb31f682b99af70
Victor Magalhaes <victor.magalhaesp@gmail.com>
2024-10-15 12:11:19 -0300

    feat: gera modelo da entidade baseado no valor do subsite

--- src/core/Traits/EntityManagerModel.php
+++ src/core/Traits/EntityManagerModel.php
@@ -235,6 +235,7 @@ trait EntityManagerModel {
 
                 $now = new \DateTime('now');
                 $newPhase->createTimestamp = $now;
+                $newPhase->subsite = $phase->subsite;
 
                 $newPhase->save(true);
 
